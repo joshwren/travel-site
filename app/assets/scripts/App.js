09d5a98fff372c51e2e4f45602b9cb2755659106
Josh <joshuawren@outlook.com>
2020-03-08 11:45:37 +1100

    All files commited 8 March 2020

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,6 @@
+import '../styles/styles.css'
+// alert("Hello, this is just a test!!!!!!!!") // Test alert for the initial configuration
+
+if (module.hot) {
+    module.hot.accept()
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// This is the location of the file we want Webpack to watch
+const path = require('path')
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    }, // This is to create the bundled.js file
+
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
+
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    } // This is for Webpack to understand css files
+}
