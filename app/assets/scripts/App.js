6e74360d61d8fb247ee41d27928bada0e405d794
Josh <joshuawren@outlook.com>
2020-04-13 10:17:17 +1000

    Java Script additions

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
-import '../styles/styles.css'
+import '../styles/styles.css' // This adds in our CSS //
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
+
 // alert("Hello, this is just a test!!!!!!!!") // Test alert for the initial configuration
 
 if (module.hot) {
