1aa542acdf978309764961ec163a2b098d033891
Josh <joshuawren@outlook.com>
2020-04-25 10:41:12 +1000

    Sticky Header changes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,10 @@
 import '../styles/styles.css' // This adds in our CSS //
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
 // Create an Object for our imported js //
-
+let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
