0ca9e8e70c1c5243228127b4461b11ece21b1b7a
Josh <joshuawren@outlook.com>
2020-04-26 08:21:47 +1000

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -9,6 +9,22 @@ new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 let mobileMenu = new MobileMenu();
+let modal
+
+// This will load in the Modal js only when clicked so it doesnt auto load //
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+      e.preventDefault()
+      if (typeof modal == "undefined") {
+        import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+          modal = new x.default()
+          setTimeout(() => modal.openTheModal(), 20)
+        }).catch(() => console.log("There was a problem."))
+      } else {
+        modal.openTheModal()
+      }
+    })
+  })
 
 // alert("Hello, this is just a test!!!!!!!!") // Test alert for the initial configuration
 
