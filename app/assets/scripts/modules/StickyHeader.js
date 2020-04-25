9fd2dd8a042b39a4ae7f2fab03a33e198056b35c
Josh <joshuawren@outlook.com>
2020-04-25 15:47:21 +1000

    Completed sticky header functionality

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,17 +1,25 @@
 import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
 
 class StickyHeader {
   constructor() {
     this.siteHeader = document.querySelector(".site-header")
     this.pageSections = document.querySelectorAll(".page-section")
+    this.browserHeight = window.innerHeight
+    this.previousScrollY = window.scrollY
     this.events()
   }
 
   events() {
     window.addEventListener("scroll", throttle(() => this.runOnScroll(), 200))
+    window.addEventListener("resize", debounce(() => {
+      this.browserHeight = window.innerHeight
+    }, 333))
   }
 
   runOnScroll() {
+    this.determineScrollDirection()
+
     if (window.scrollY > 60) {
       this.siteHeader.classList.add("site-header--dark")
     } else {
@@ -20,9 +28,17 @@ class StickyHeader {
 
     this.pageSections.forEach(el => this.calcSection(el))
   }
-}
 
-calcSection(el) {
+  determineScrollDirection() {
+    if (window.scrollY > this.previousScrollY) {
+      this.scrollDirection = 'down'
+    } else {
+      this.scrollDirection = 'up'
+    }
+    this.previousScrollY = window.scrollY
+  }
+
+  calcSection(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
       let scrollPercent = el.getBoundingClientRect().top / this.browserHeight * 100
       if (scrollPercent < 18 && scrollPercent > -0.1 && this.scrollDirection == 'down' || scrollPercent < 33 && this.scrollDirection == 'up') {
